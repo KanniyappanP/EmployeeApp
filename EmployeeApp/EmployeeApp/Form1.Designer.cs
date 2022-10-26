1db2e416bb31b0d61a56e0fa7065ced54c00b228
KanniyappanP <56436173+KanniyappanP@users.noreply.github.com>
2022-10-26 17:58:08 +0530

    Add files via upload
    EmployeeApp file changes has been committed.

---
+++ EmployeeApp/EmployeeApp/Form1.Designer.cs
@@ -0,0 +1,227 @@
+﻿
+namespace EmployeeApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.empName = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.empAddress = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.empLocation = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.empLocationTextBox = new System.Windows.Forms.TextBox();
+            this.empAddressTextBox = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.empNameTextBox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnEdit = new System.Windows.Forms.Button();
+            this.btnUpdate = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.empName,
+            this.empAddress,
+            this.empLocation});
+            this.dataGridView1.Location = new System.Drawing.Point(44, 452);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 29;
+            this.dataGridView1.Size = new System.Drawing.Size(674, 231);
+            this.dataGridView1.TabIndex = 7;
+            // 
+            // empName
+            // 
+            this.empName.HeaderText = "Name";
+            this.empName.MinimumWidth = 6;
+            this.empName.Name = "empName";
+            this.empName.ReadOnly = true;
+            this.empName.Width = 125;
+            // 
+            // empAddress
+            // 
+            this.empAddress.HeaderText = "Address";
+            this.empAddress.MinimumWidth = 6;
+            this.empAddress.Name = "empAddress";
+            this.empAddress.Width = 125;
+            // 
+            // empLocation
+            // 
+            this.empLocation.HeaderText = "Location";
+            this.empLocation.MinimumWidth = 6;
+            this.empLocation.Name = "empLocation";
+            this.empLocation.Width = 125;
+            // 
+            // btnClear
+            // 
+            this.btnClear.Location = new System.Drawing.Point(270, 268);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(102, 31);
+            this.btnClear.TabIndex = 12;
+            this.btnClear.Text = "Clear";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(135, 268);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(94, 29);
+            this.btnSave.TabIndex = 7;
+            this.btnSave.Text = "Save";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(78, 190);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(66, 20);
+            this.label3.TabIndex = 11;
+            this.label3.Text = "Location";
+            // 
+            // empLocationTextBox
+            // 
+            this.empLocationTextBox.Location = new System.Drawing.Point(259, 183);
+            this.empLocationTextBox.Name = "empLocationTextBox";
+            this.empLocationTextBox.Size = new System.Drawing.Size(181, 27);
+            this.empLocationTextBox.TabIndex = 10;
+            // 
+            // empAddressTextBox
+            // 
+            this.empAddressTextBox.Location = new System.Drawing.Point(259, 116);
+            this.empAddressTextBox.Name = "empAddressTextBox";
+            this.empAddressTextBox.Size = new System.Drawing.Size(177, 27);
+            this.empAddressTextBox.TabIndex = 9;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(78, 119);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(62, 20);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "Address";
+            // 
+            // empNameTextBox
+            // 
+            this.empNameTextBox.Location = new System.Drawing.Point(259, 51);
+            this.empNameTextBox.Name = "empNameTextBox";
+            this.empNameTextBox.Size = new System.Drawing.Size(175, 27);
+            this.empNameTextBox.TabIndex = 7;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(78, 54);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(119, 20);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Employee Name";
+            // 
+            // btnEdit
+            // 
+            this.btnEdit.Location = new System.Drawing.Point(78, 382);
+            this.btnEdit.Name = "btnEdit";
+            this.btnEdit.Size = new System.Drawing.Size(119, 33);
+            this.btnEdit.TabIndex = 13;
+            this.btnEdit.Text = "Edit";
+            this.btnEdit.UseVisualStyleBackColor = true;
+            this.btnEdit.Click += new System.EventHandler(this.btnEdit_Click);
+            // 
+            // btnUpdate
+            // 
+            this.btnUpdate.Location = new System.Drawing.Point(243, 382);
+            this.btnUpdate.Name = "btnUpdate";
+            this.btnUpdate.Size = new System.Drawing.Size(110, 33);
+            this.btnUpdate.TabIndex = 14;
+            this.btnUpdate.Text = "Update";
+            this.btnUpdate.UseVisualStyleBackColor = true;
+            this.btnUpdate.Click += new System.EventHandler(this.btnUpdate_Click);
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Location = new System.Drawing.Point(395, 382);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(109, 33);
+            this.btnDelete.TabIndex = 15;
+            this.btnDelete.Text = "Delete";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(925, 729);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.btnUpdate);
+            this.Controls.Add(this.btnEdit);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.empLocationTextBox);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.empAddressTextBox);
+            this.Controls.Add(this.empNameTextBox);
+            this.Controls.Add(this.label2);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox empLocationTextBox;
+        private System.Windows.Forms.TextBox empAddressTextBox;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox empNameTextBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnEdit;
+        private System.Windows.Forms.Button btnUpdate;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.DataGridViewTextBoxColumn empName;
+        private System.Windows.Forms.DataGridViewTextBoxColumn empAddress;
+        private System.Windows.Forms.DataGridViewTextBoxColumn empLocation;
+    }
+}
+
---
+++ EmployeeApp/EmployeeApp/Form1.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace EmployeeApp
+{
+    public partial class Form1 : Form
+    {
+        int id = 0;
+
+        public List<Employee> Employees { get; set; }
+
+        public Form1()
+        {
+            InitializeComponent();
+            this.Employees = new List<Employee>();
+            this.dataGridView1.AutoGenerateColumns = false;
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.empName.DataPropertyName = "Name";
+            this.empAddress.DataPropertyName = "Address";
+            this.empLocation.DataPropertyName = "Location";
+            this.dataGridView1.SelectionMode = DataGridViewSelectionMode.FullRowSelect;
+
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            Employee employee = new Employee();
+            employee.ID = id + 1;
+            employee.Name = empNameTextBox.Text;
+            employee.Address = empAddressTextBox?.Text;
+            employee.Location = empLocationTextBox.Text;
+            this.Employees.Add(employee);
+            DisplayDataInGrid();
+            id = id + 1;
+            ResetValues();
+        }
+
+        private void btnEdit_Click(object sender, EventArgs e)
+        {
+            var employee = this.dataGridView1.SelectedRows[0]?.DataBoundItem as Employee;
+            if (employee != null)
+            {
+                this.ResetValues();
+                this.empNameTextBox.Text = employee.Name;
+                this.empAddressTextBox.Text = employee.Address;
+                this.empLocationTextBox.Text = employee.Location;
+                this.btnSave.Enabled = false;
+            }
+        }
+
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            if (this.Employees != null && this.Employees.Count > 0)
+            {
+                var selectedData = this.dataGridView1.SelectedRows[0].DataBoundItem;
+                this.Employees.Remove(selectedData as Employee);
+                this.DisplayDataInGrid();
+                MessageBox.Show("Deleted the selected employee detail successfully");
+            }
+            else
+            {
+                MessageBox.Show("No records found to perform delete operation!!!");
+            }
+        }
+
+        private void btnUpdate_Click(object sender, EventArgs e)
+        {
+
+            var employee = this.dataGridView1.SelectedRows[0].DataBoundItem as Employee;
+            if (employee != null && this.Employees != null && this.Employees.Count > 0)
+            {
+                Employee updateEmpData = this.Employees.Find(x => x.ID == employee.ID);
+                var selectedIndex = this.Employees.IndexOf(employee);
+                if (updateEmpData.Name != empNameTextBox.Name)
+                {
+                    updateEmpData.Name = this.empNameTextBox.Text;
+                }
+                if (updateEmpData.Address != empAddressTextBox.Text)
+                {
+                    updateEmpData.Address = this.empAddressTextBox.Text;
+                }
+                if (updateEmpData.Location != empLocationTextBox.Text)
+                {
+                    updateEmpData.Location = this.empLocationTextBox.Text;
+                }
+
+                this.DisplayDataInGrid();
+                this.ResetValues();
+                MessageBox.Show("Updated the employee details successfully");
+            }
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            this.ResetValues();
+        }
+
+        private void DisplayDataInGrid()
+        {
+            var bindingList = new BindingList<Employee>(this.Employees);
+            var source = new BindingSource(bindingList, null);
+            this.dataGridView1.DataSource = source;
+        }
+
+        private void ResetValues()
+        {
+            this.empNameTextBox.Text = string.Empty;
+            this.empAddressTextBox.Text = string.Empty;
+            this.empLocationTextBox.Text = string.Empty;
+            this.btnSave.Enabled = true;
+        }  
+    }
+
+    public class Employee
+    {
+        public int ID { get; set; }
+         
+        public string Name { get; set; }
+
+        public string Address { get; set; }
+
+        public string Location { get; set; }
+    }
+}
